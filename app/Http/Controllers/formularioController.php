b160a263f046700d4cfb853837bd00d7acd7fefb
“Juan” <jcasinoste16dw@ikzubirimanteo.com>
2017-11-21 13:18:10 +0100

    añadir middleware al controlador

--- app/Http/Controllers/formularioController.php
+++ app/Http/Controllers/formularioController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class formularioController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('perm');
+    }
+
+    public function formulario()
+    {
+        return view('vistaPermiso');
+    }
 }
