f2bc4a42de57937a923e5c09d556a9a0303d97e7
“Juan” <jcasinoste16dw@ikzubirimanteo.com>
2017-11-21 12:53:17 +0100

    rutas creadas

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,13 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+Route::get('/', function () {
+    return view('landingPage');
+});
+
+Route::post('/formulario', 'formularioController@formulario');
