cd21dafe2d44b100b46b19e37ef33de472d9d15f
“Juan” <jcasinoste16dw@ikzubirimanteo.com>
2017-11-21 12:40:02 +0100

    modelo creado

---
+++ app/UsuPermitido.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UsuPermitido extends Model
+{
+    use Notifiable;
+
+    protected $table = 'usuPermitidos';
+
+    protected $fillable = ['nombreUsu',];
+}
