4f106990142845483f7563fd70d931bdf50aa5b4
“Juan” <jcasinoste16dw@ikzubirimanteo.com>
2017-11-21 13:03:59 +0100

    creadas vistas permiso

---
+++ resources/views/vistaPermiso.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>vista permiso</title>
+
+    </head>
+    <body>
+        <h1>El usuario <?php echo $_POST["usuario"]; ?> tiene permiso</h1>
+    </body>
+</html>
